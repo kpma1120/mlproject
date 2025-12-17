dc502e3c1cdc1393e569886ff39250c6c7e4ada8
Vincent Ma <kpma1120@yahoo.com.hk>
2025-12-17 20:29:14 +0800

    deployment configuration for AWS Beanstalk

--- app.py
+++ app.py
@@ -42,5 +42,5 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
